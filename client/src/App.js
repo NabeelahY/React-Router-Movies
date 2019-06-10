a26d9dfc20ca3d399e013c91eebcdf0ca8c44764
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-10 12:31:58 +0100

    Create routes in app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default class App extends Component {
   constructor() {
@@ -17,13 +18,18 @@ export default class App extends Component {
     savedList.push(movie);
     this.setState({ savedList });
   };
+  
 
   render() {
+    {console.log(this.state.savedList)}
     return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
+      <Router>
+        <div>
+          <SavedList list={this.state.savedList} />
+          <Route exact path='/' component={MovieList} />
+          <Route path='/movies/:id' component={Movie} />
+        </div>
+      </Router>
     );
   }
 }
